1119d3d1da6d50caa4968a8b8aacbbce68b8e019
JeremKy <jeremky@me.com>
2026-08-21 00:19:34 +0200

    Create outline.scm

---
+++ languages/vim/outline.scm
@@ -0,0 +1,13 @@
+(function_definition
+  (function_declaration
+    name: (identifier) @name)) @item
+
+(for_loop
+  variable: (identifier) @name) @item
+
+(let_statement
+  (identifier) @name) @item
+
+(autocmd_statement
+  (au_event_list) @context
+  (pattern) @name) @item
