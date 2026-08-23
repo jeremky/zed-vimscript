609504427498c25805d985540c705d9cfad8ffd0
JeremKy <jeremky@me.com>
2026-08-23 10:32:12 +0200

    Fix outline to recognize scoped identifiers
    function_declaration and let_statement names are scoped_identifier
    nodes (g:, s:, b:...) rather than plain identifier in the common case,
    so scoped functions/variables were missing from the outline, and
    let_statement could even pick up the RHS identifier as the name.

--- languages/vim/outline.scm
+++ languages/vim/outline.scm
@@ -1,12 +1,13 @@
 (function_definition
   (function_declaration
-    name: (identifier) @name)) @item
+    name: [(identifier) (scoped_identifier)] @name)) @item
 
 (for_loop
   variable: (identifier) @name) @item
 
 (let_statement
-  (identifier) @name) @item
+  .
+  [(identifier) (scoped_identifier)] @name) @item
 
 (autocmd_statement
   (au_event_list) @context
